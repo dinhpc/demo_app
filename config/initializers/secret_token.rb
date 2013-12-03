d1b79784ab8350afb16bf6a5af2098784b0135b0
Dinhpc <framgia-user@framgiauser-Inspiron-5520.(none)>
2013-12-03 11:06:47 +0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DemoApp::Application.config.secret_key_base = '85a5b32df66abd1ed5030032cd51767de3dc8daf52d53768268425b407762c45f95ab5ba4d12dc1e28f8dd59918c69669cbdc1be08fd1addb9112c05a2685b20'
